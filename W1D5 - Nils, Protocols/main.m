bf40c5b349c78f3cd69458937be63897262bd436
Daniel Mathews <dannyl.mathews@gmail.com>
2015-05-07 22:47:40 -0700

    Initial Commit

---
+++ W1D5 - Nils, Protocols/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D5 - Nils, Protocols
+//
+//  Created by Daniel Mathews on 2015-05-07.
+//  Copyright (c) 2015 ca.lighthouselabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
